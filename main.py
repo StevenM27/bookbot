8456bf28e75c3eece506f77ea6f1e8aff3d5a98c
StevenM27 <83900183+StevenM27@users.noreply.github.com>
2025-06-01 19:47:29 -0700

    Add functions to count the characters and sort them from most occurences to least occurences

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_character_counts
 
 def get_book_text(filepath):
 
@@ -11,15 +11,28 @@ def get_book_text(filepath):
 
 
 def main():
+
+    filepath = "books/frankenstein.txt"
     
-    content = get_book_text("./books/frankenstein.txt")
+    content = get_book_text(filepath)
     num_words = count_words(content)
 
-    print(f"{num_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
 
     char_dict = count_characters(content)
+    sorted_chars = sort_character_counts(char_dict)
 
-    print(char_dict)
+    print("--------- Character Count -------")
+
+    for entry in sorted_chars:
+        if entry["char"].isalpha():
+            print(f"{entry["char"]}: {entry["num"]}")
+
+    print("============= END ===============")
+
 
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,21 @@ def count_characters(content):
             character_dict[char] = 1
 
     return character_dict
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def sort_character_counts(char_dict):
+    separated_dicts = []
+
+    for key in char_dict:
+        separated_dicts.append({
+            "char": key,
+            "num": char_dict[key]
+        })
+
+    separated_dicts.sort(reverse=True, key=sort_on)
+
+    return separated_dicts
